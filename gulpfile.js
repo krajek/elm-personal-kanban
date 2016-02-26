99e0effed60bc3853c3880b72ecc4c983b76ffb6
Artur Krajewski <krajek.dev@gmail.com>
2016-02-26 22:01:01 +0100

    Added elm as node dependency, removed elm-init task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var elm  = require('gulp-elm');
  
-gulp.task('elm-init', elm.init);
- 
-gulp.task('elm', ['elm-init'], function(){
+gulp.task('elm', function(){
   return gulp.src('elm-src/Main.elm')
     .pipe(elm())	
     .pipe(gulp.dest('.'));
